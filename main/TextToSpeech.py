422c7c0932c2d54915a1e7957004196ec12791e3
MohammadrezaAmani <more.amani@yahoo.com>
2022-02-10 17:57:26 +0330

    add first files and folders

---
+++ main/TextToSpeech.py
@@ -0,0 +1,9 @@
+#-------------------------Text To Speech with GUI-----------------------------
+# author: Mohammadreza Amani
+# GitHub: https://www.github.com/MohammadrezaAmani
+# Linkedin: https://www.linkedin.com/in/mohammadreza-amani/
+# Date: 2022/01/31
+
+#---------------------------------logic----------------------------------------
+# get the text from the user and convert it to speech 
+# using gtts

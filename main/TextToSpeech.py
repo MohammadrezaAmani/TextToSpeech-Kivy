ea4386a67acba1af13a03bbac1affad23906bb03
MohammadrezaAmani <more.amani@yahoo.com>
2023-09-14 19:16:29 +0330

    add functionality

--- main/TextToSpeech.py
+++ main/TextToSpeech.py
@@ -1,57 +1,63 @@
-#-------------------------Text To Speech with GUI-----------------------------
+# -------------------------Text To Speech with GUI-----------------------------
 # author: Mohammadreza Amani
 # GitHub: https://www.github.com/MohammadrezaAmani
 # Linkedin: https://www.linkedin.com/in/mohammadreza-amani/
-# Date: 2022/01/31
+# Date: 2022/01/31 - Updated_at: Sep/14/2023
 
-#---------------------------------logic----------------------------------------
+# ---------------------------------logic----------------------------------------
 # get the text from the user and convert it to speech
 # using gtts
-#------------------------------import libraries--------------------------------
+# ------------------------------import libraries--------------------------------
 
-#importing some required libraries for frontend
+# importing some required libraries for frontend
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.core.window import Window
 from kivymd.uix.list import OneLineIconListItem
 
-#importing some required libraries for backend
+# importing some required libraries for backend
 import gtts
 from playsound import playsound
 import os
 import datetime
-#seting the window size
+
+# seting the window size
 Window.size = (360, 640)
 
-#kv code for frontend
+# kv code for frontend
 text = """
-MDGridLayout:
-    cols: 1
-    rows: 2
+BoxLayout:
+    orientation: 'vertical'
+    MDTopAppBar:
+        title: "Text to Speech"
+        elevation: 10
     ScrollView:
         MDList:
-            id: list          
-    MDBoxLayout:
-        cols: 3
-        rows: 1
+            id: list
+    BoxLayout:
+        orientation: 'horizontal'
         size_hint_y: None
-        height: self.minimum_height
+        height: dp(50)
         padding: dp(10)
         spacing: dp(10)
         MDTextField:
             hint_text: "Your text here"
             id: text_field
             multiline: True
-            on_text_validate: app.on_text_validate(self.text)
         MDRaisedButton:
-            text: "save"
+            text: "Clear"
+            on_release: app.clear_text()
+        MDRaisedButton:
+            text: "Save"
             on_release: app.save()
         MDRaisedButton:
             text: "Speak"
             on_release: app.speak()
 """
 
-#creating the class for the frontend
+
+# creating the class for the frontend
+
 
 class MYAPP(MDApp):
     def build(self):
@@ -59,20 +65,23 @@ class MYAPP(MDApp):
         self.theme_cls.primary_hue = "500"
         self.title = "Text to Speech"
         return Builder.load_string(text)
-    
-    #function for find the name of the file
+
+    # function for find the name of the file
     def name(self, text):
-        return "{}{}.mp3".format(text,datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
-    #function for the save button
+        return "{}{}.mp3".format(
+            text, datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+        )
+
+    # function for the save button
 
     def save(self):
         if self.root.ids.text_field.text != "":
             print(self.root.ids.text_field.text)
-            #converting the text to speech
-            tts = gtts.gTTS(text=self.root.ids.text_field.text,
-                            lang='en', slow=False)
+            # converting the text to speech
+            tts = gtts.gTTS(text=self.root.ids.text_field.text, lang="en", slow=False)
             self.root.ids.list.add_widget(
-                OneLineIconListItem(text=self.root.ids.text_field.text))
+                OneLineIconListItem(text=self.root.ids.text_field.text)
+            )
             # name of file :
             text = ""
             if len(self.root.ids.text_field.text) > 20:
@@ -84,20 +93,20 @@ class MYAPP(MDApp):
                 text = self.root.ids.text_field.text
             text = self.name(text)
             self.root.ids.text_field.text = ""
-            #saving the file
+            # saving the file
             tts.save(text)
-            #playing the file
+            # playing the file
             return text
-        
+
     # function for the text_validate button
     def speak(self):
         try:
             playsound(self.save())
         except:
             print("invalid text")
-            self.root.ids.text_field.text = "" 
-    
+            self.root.ids.text_field.text = ""
 
-#creating the instance of the class and running the app
+
+# creating the instance of the class and running the app
 if __name__ == "__main__":
     MYAPP().run()
